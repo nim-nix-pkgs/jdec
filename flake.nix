68dfce569f3757e0569488eb33361ca191a617cb
hugosenari <hugosenari@gmail.com>
2022-05-12 11:53:01 -0300

    chore: re index jdec

--- flake.nix
+++ flake.nix
@@ -7,6 +7,30 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
+  inputs."jdec-develop".dir   = "develop";
+  inputs."jdec-develop".owner = "nim-nix-pkgs";
+  inputs."jdec-develop".ref   = "master";
+  inputs."jdec-develop".repo  = "jdec";
+  inputs."jdec-develop".type  = "github";
+  inputs."jdec-develop".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."jdec-develop".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."jdec-master".dir   = "master";
+  inputs."jdec-master".owner = "nim-nix-pkgs";
+  inputs."jdec-master".ref   = "master";
+  inputs."jdec-master".repo  = "jdec";
+  inputs."jdec-master".type  = "github";
+  inputs."jdec-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."jdec-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."jdec-v0_1_0".dir   = "v0_1_0";
+  inputs."jdec-v0_1_0".owner = "nim-nix-pkgs";
+  inputs."jdec-v0_1_0".ref   = "master";
+  inputs."jdec-v0_1_0".repo  = "jdec";
+  inputs."jdec-v0_1_0".type  = "github";
+  inputs."jdec-v0_1_0".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."jdec-v0_1_0".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
   let 
     lib  = flakeNimbleLib.lib;
